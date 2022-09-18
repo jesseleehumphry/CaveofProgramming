eeaff93f9ae952a3d4dffe2a05f2b6a5a8e29599
Jesse Lee Humphry <jesse.humphry@gmail.com>
2022-09-18 14:28:34 +0700

    Saving binaries
    Implemented binary filesaving in c++

--- src/3/3 - 5 Reading and Writing Binaries/ReadWriteBinaries.cpp
+++ src/3/3 - 5 Reading and Writing Binaries/ReadWriteBinaries.cpp
@@ -15,6 +15,8 @@ int main()
 {
     Person person = {"Jane",28,45};
 
+    /////////////////////////////////////////////////////////////////////
+    // Write Binary File
 
     string filename = "test.bin";
     fstream output;
@@ -24,6 +26,7 @@ int main()
 
     if (output.is_open())
     {
+        // When writing, the data will be pulled from the struct reference
         output.write(reinterpret_cast<char*>(&person), sizeof(Person));
 
         output.close();
@@ -32,6 +35,29 @@ int main()
     {
         cout << "Could not create file " << filename << endl;
     }
+
+    /////////////////////////////////////////////////////////////////////
+    // Read Binary File
+
+    Person someoneElse = {};
+
+    fstream input;
+
+    input.open(filename, ios::binary|ios::in);
+
+    if (input.is_open())
+    {
+        // When reading, the data will be put to the struct reference
+        input.read(reinterpret_cast<char*>(&someoneElse), sizeof(Person));
+
+        cout << someoneElse.name << endl;
+        input.close();
+    }
+    else
+    {
+        cout << "Could not read file " << filename << endl;
+    }
+
 
 
     return 0;
